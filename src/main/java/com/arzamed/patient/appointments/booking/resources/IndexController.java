ac8f3d0f0ae42405bdb72bb2e8053c0d95c8683e
Rana Das <rana.pratap.das@gmail.com>
2019-09-02 20:42:31 +0100

    Adding Web module

---
+++ src/main/java/com/arzamed/patient/appointments/booking/resources/IndexController.java
@@ -0,0 +1,19 @@
+package com.arzamed.patient.appointments.booking.resources;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+    @Value("${spring.application.name}")
+    private String appName ;
+    //= "";
+
+    @GetMapping("/")
+    public String homePage(Model model) {
+        model.addAttribute("appName", appName);
+        return "home";
+    }
+}
