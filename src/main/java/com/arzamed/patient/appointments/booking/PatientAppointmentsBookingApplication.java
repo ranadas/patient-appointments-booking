1caf6f673374bea3f8abeedc044ff0ee22870345
alberto <a.ghiribaldi@arzadv.com>
2019-09-02 17:26:50 +0200

    initial commit
    Signed-off-by: alberto <a.ghiribaldi@arzadv.com>

---
+++ src/main/java/com/arzamed/patient/appointments/booking/PatientAppointmentsBookingApplication.java
@@ -0,0 +1,13 @@
+package com.arzamed.patient.appointments.booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientAppointmentsBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientAppointmentsBookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arzamed/patient/appointments/booking/PatientAppointmentsBookingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.arzamed.patient.appointments.booking;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PatientAppointmentsBookingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
